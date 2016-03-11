61da3247d2b9f6490d5c26dacda97b1558add42d
Kody Buss <kody.buss@gmail.com>
2016-03-10 21:48:53 -0600

    Added textures and bump mapping to the floor Added a fog  and spotlight

--- Assets/Scripts/GUIFollow.cs
+++ Assets/Scripts/GUIFollow.cs
@@ -3,10 +3,11 @@ using System.Collections;
 
 public class GUIFollow : MonoBehaviour {
 	public Transform Target;
+	public float followHeight = 2.5f;
 
 	// Update is called once per frame
 	void LateUpdate () {
-		transform.position = new Vector3 (Target.position.x, 2.5f, Target.position.z);
+		transform.position = new Vector3 (Target.position.x, followHeight, Target.position.z);
 
 	}
 }
